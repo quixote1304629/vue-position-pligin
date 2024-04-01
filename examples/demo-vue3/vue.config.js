aa36c3a9906cda3db408b79c14eff7d3ab176f43
liyang.jim <liyang.jim@bigo.sg>
2024-04-01 19:38:49 +0800

    1

--- examples/demo-vue3/vue.config.js
+++ examples/demo-vue3/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const { defineConfig } = require('@vue/cli-service');
 // const VueInspectorPlugin = require('vue-inspector-plugin/webpack').default;
-const VuePositionPlugin = require('./plugins/index.js').default;
+const VuePositionPlugin = require('./position-plugin/index.js').default;
 
 module.exports = defineConfig({
   transpileDependencies: true,
