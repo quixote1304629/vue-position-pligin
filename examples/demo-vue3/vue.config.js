ec4f4ba74a38728d8a89e7ee71602225e977427e
liyang.jim <liyang.jim@bigo.sg>
2024-04-02 17:54:36 +0800

    1

--- examples/demo-vue3/vue.config.js
+++ examples/demo-vue3/vue.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const { defineConfig } = require('@vue/cli-service');
-// const VueInspectorPlugin = require('vue-inspector-plugin/webpack').default;
 const VuePositionPlugin = require('./position-plugin/index.js').default;
 
 module.exports = defineConfig({
@@ -11,7 +10,6 @@ module.exports = defineConfig({
   },
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
-      // config.plugin('vue-inspector-plugin').use(VueInspectorPlugin());
       config.plugin('vue-position-plugin').use(new VuePositionPlugin());
     }
   },
